500ba2241eb73cd97c8bb63b1a9df580ba83cc4e
Hlavutelomdu <Hlavutelomdu@gmail.com>
2022-07-05 11:17:16 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,7 @@ int print_last_digit(int n)
 {
 	int a;
 
-	if (n < o)
+	if (n < 0)
 		n = -n;
 
 	a = n % 10;
@@ -17,7 +17,7 @@ int print_last_digit(int n)
 	if (a < 0)
 		a = -a;
 
-	-putchar(a + '0');
+	_putchar(a + '0');
 
 	return (a);
 }
