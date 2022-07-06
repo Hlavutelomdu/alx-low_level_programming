e171c28120eab76f12afcbe83d1ea776fc274cca
Hlavutelomdu <Hlavutelomdu@gmail.com>
2022-07-06 07:53:22 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(n % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", n, ',', ' ');
 		}
 	}
 	else if (n == 98)
