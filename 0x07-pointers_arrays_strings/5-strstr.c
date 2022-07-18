16bc080dbcc18acef80924292d355eae193c9acb
Hlavutelomdu <Hlavutelomdu@gmail.com>
2022-07-18 08:02:20 -0700

    strstr fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,11 +17,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] != needle[j])
-			       break;
+			if (haystack[i + j] != needlee[j])
+				break;
 		}
 	if (!needle[j])
-	return (&haystack[i]);
+		return (&haystack[i]);
 	}
-return (NULL);
-};
+	return (NULL);
+}
